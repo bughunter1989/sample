aef0d4a80d2c2c4f80b69554cb7ae0cb529380e5
bughunter1989 <nahangpolo+bb@gmail.com>
2026-07-13 13:35:03 +0000

    added sqlite

--- cmd/web-api/main.go
+++ cmd/web-api/main.go
@@ -1,7 +1,40 @@
-package main 
+package main
 
-import "fmt"
+import (
+  "context"
+  "gorm.io/driver/sqlite"
+  "gorm.io/gorm"
+)
 
-func main(){
-	fmt.Println("Hello there")
+type Product struct {
+  gorm.Model
+  Code  string
+  Price uint
+}
+
+func main() {
+  db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+  if err != nil {
+    panic("failed to connect database")
+  }
+
+  ctx := context.Background()
+
+  // Migrate the schema
+  db.AutoMigrate(&Product{})
+
+  // Create
+  err = gorm.G[Product](db).Create(ctx, &Product{Code: "D42", Price: 100})
+
+  // Read
+  product, err := gorm.G[Product](db).Where("id = ?", 1).First(ctx) // find product with integer primary key
+  products, err := gorm.G[Product](db).Where("code = ?", "D42").Find(ctx) // find product with code D42
+
+  // Update - update product's price to 200
+  err = gorm.G[Product](db).Where("id = ?", product.ID).Update(ctx, "Price", 200)
+  // Update - update multiple fields
+  err = gorm.G[Product](db).Where("id = ?", product.ID).Updates(ctx, Product{Code: "D42", Price: 100})
+
+  // Delete - delete product
+  err = gorm.G[Product](db).Where("id = ?", product.ID).Delete(ctx)
 }
